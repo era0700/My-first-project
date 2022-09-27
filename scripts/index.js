06bc795e14ab987855fd1de1bbe4a5ac5127dd05
era0700 <myrtezaera@gmail.com>
2022-09-27 09:41:42 +0200

    Project merge

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/uniqueorns-cffea";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDDITP852vTbvXIhw8qCnsDoRBVdXQ9S1E",
+    authDomain: "uniqueorns-cffea.firebaseapp.com",
+    projectId: "uniqueorns-cffea",
+    storageBucket: "uniqueorns-cffea.appspot.com",
+    messagingSenderId: "674836566075",
+    appId: "1:674836566075:web:eba3ecd8d64e72444fcc1e",
+    measurementId: "G-F2KCVW5GVP"
+  };
